2eca75d20a5322b8c7be2d4b2db366cf44d3f973
alenissmart <alen112.scientist@gmail.com>
2026-03-09 20:56:22 -0400

    Added screenTemplate to create a standard template for every screen. Also completed all auth screens and onboard screens index and personalInfo

--- app/(onboarding)/index.tsx
+++ app/(onboarding)/index.tsx
@@ -1,62 +1,80 @@
-import { LinearGradient } from 'expo-linear-gradient';
 import { router } from 'expo-router';
 import React from 'react';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Image, Pressable, StyleSheet, Text } from 'react-native';
+import AuthScreenLayout from '../screenTemplate';
 
 const onboardingWelcome = () => {
   return (
-    <View style={styles.container}>
-      <LinearGradient colors={['#F5B3B6', '#C94B52']} style={styles.gradient}>
-        <Text style={styles.title}>Onboarding Welcome Template</Text>
-
-        <Pressable
-          onPress={() => {
-            // add function to save user input
-            router.push('/(onboarding)/personalInfoScreen');
-          }}
-          style={({ pressed }) => [
-            styles.nextButtonStyle,
-            {
-              transform: pressed ? [{ scale: 0.95 }] : [{ scale: 1 }],
-            },
-          ]}
-        >
-          <Text style={styles.mainButtonText}>Start Onboarding</Text>
-        </Pressable>
-      </LinearGradient>
-    </View>
+    <AuthScreenLayout
+      headerContent={
+        <>
+          <Text style={styles.title}>{'Let’s get started'}</Text>
+          <Text style={styles.aboutText}>
+            We will collect information about your preferences and schedule in
+            the upcoming screens
+          </Text>
+        </>
+      }
+      middleContent={
+        <>
+          <Image
+            source={require('../../assets/images/tempLogo.png')}
+            style={styles.big_logo}
+          />
+        </>
+      }
+      bottomContent={
+        <>
+          <Pressable
+            onPress={() => {
+              // add function to save user input
+              router.push('/(onboarding)/personalInfoScreen');
+            }}
+            style={({ pressed }) => [
+              styles.onboardButton,
+              {
+                transform: pressed ? [{ scale: 0.95 }] : [{ scale: 1 }],
+              },
+            ]}
+          >
+            <Text style={styles.onboardText}>Onboard</Text>
+          </Pressable>
+        </>
+      }
+    />
   );
 };
 
 export default onboardingWelcome;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-  },
   title: {
     color: 'black',
-    fontSize: 35,
+    fontSize: 40,
     textAlign: 'center',
     fontWeight: '600',
-    marginBottom: 50,
   },
-  gradient: {
-    flex: 1,
-    padding: 15,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 5,
+  aboutText: {
+    color: 'rgba(0, 0, 0, .6)',
+    fontSize: 16,
+    padding: 20,
+    textAlign: 'center',
   },
-  nextButtonStyle: {
+  big_logo: {
+    width: 200,
+    height: 200,
+    borderRadius: 15,
+    marginTop: 80,
+    marginBottom: 100,
+  },
+  onboardButton: {
     width: 200,
     height: 60,
     borderRadius: 15,
     backgroundColor: 'rgba(163,51,58,1)',
     justifyContent: 'center',
   },
-  mainButtonText: {
+  onboardText: {
     color: 'white',
     fontSize: 20,
     textAlign: 'center',
--- app/(onboarding)/personalInfoScreen.tsx
+++ app/(onboarding)/personalInfoScreen.tsx
@@ -1,76 +1,112 @@
-import { LinearGradient } from 'expo-linear-gradient';
 import { router } from 'expo-router';
 import React from 'react';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, TextInput } from 'react-native';
+import AuthScreenLayout from '../screenTemplate';
 
 const presonalInfoScreen = () => {
   return (
-    <View style={styles.container}>
-      <LinearGradient colors={['#F5B3B6', '#C94B52']} style={styles.gradient}>
-        <Text style={styles.title}>presonal Info Screen Template</Text>
+    <AuthScreenLayout
+      headerContent={
+        <>
+          <Text style={styles.title}>Tell us about you</Text>
+          <Text style={styles.aboutText}>
+            {'We’ll use this to personalize your schedule'}
+          </Text>
+        </>
+      }
+      middleContent={
+        <>
+          <TextInput
+            style={styles.inputs}
+            placeholder="Name"
+            placeholderTextColor="rgba(180, 180, 180, 1)"
+            keyboardType="default"
+          />
 
-        <Pressable
-          onPress={() => {
-            // add function to save user input
-            router.push('/(onboarding)/preferencesScreen');
-          }}
-          style={({ pressed }) => [
-            styles.nextButtonStyle,
-            {
-              transform: pressed ? [{ scale: 0.95 }] : [{ scale: 1 }],
-            },
-          ]}
-        >
-          <Text style={styles.mainButtonText}>Done</Text>
-        </Pressable>
+          <TextInput
+            style={styles.inputs}
+            placeholder="Age"
+            placeholderTextColor="rgba(180, 180, 180, 1)"
+            keyboardType="number-pad"
+          />
 
-        <Pressable
-          onPress={() => {
-            // function to save user input
-            router.back();
-          }}
-        >
-          {({ pressed }) => (
-            <Text
-              style={[
-                styles.smallText,
-                {
-                  color: pressed
-                    ? 'rgba(91, 90, 90, 1)'
-                    : 'rgba(182, 179, 179, 1)',
-                  marginTop: 10,
-                },
-              ]}
-            >
-              Back
-            </Text>
-          )}
-        </Pressable>
-      </LinearGradient>
-    </View>
+          <TextInput
+            style={styles.inputs}
+            placeholder="When do you feel most productive?"
+            placeholderTextColor="rgba(180, 180, 180, 1)"
+            keyboardType="default"
+          />
+        </>
+      }
+      bottomContent={
+        <>
+          <Pressable
+            onPress={() => {
+              // add function to save user input
+              router.replace('/(onboarding)/preferencesScreen');
+            }}
+            style={({ pressed }) => [
+              styles.nextButtonStyle,
+              {
+                transform: pressed ? [{ scale: 0.95 }] : [{ scale: 1 }],
+              },
+            ]}
+          >
+            <Text style={styles.mainButtonText}>Next</Text>
+          </Pressable>
+          <Pressable
+            onPress={() => {
+              router.back();
+            }}
+          >
+            {({ pressed }) => (
+              <Text
+                style={[
+                  styles.smallText,
+                  {
+                    color: pressed
+                      ? 'rgba(91, 90, 90, 1)'
+                      : 'rgba(182, 179, 179, 1)',
+                    marginTop: 10,
+                  },
+                ]}
+              >
+                Back
+              </Text>
+            )}
+          </Pressable>
+        </>
+      }
+    />
   );
 };
 
 export default presonalInfoScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-  },
   title: {
     color: 'black',
-    fontSize: 35,
+    fontSize: 40,
     textAlign: 'center',
     fontWeight: '600',
-    marginBottom: 50,
   },
-  gradient: {
-    flex: 1,
-    padding: 15,
+  aboutText: {
+    color: 'rgba(0, 0, 0, .6)',
+    fontSize: 16,
+    padding: 20,
+    textAlign: 'center',
+    marginBottom: 60,
+  },
+  inputs: {
     alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 5,
+    height: 52,
+    width: '100%',
+    maxWidth: 354,
+    padding: 20,
+    backgroundColor: 'rgba(255, 255, 255, 1)',
+    borderRadius: 15,
+    color: 'rgba(0,0,0,1)',
+    marginBottom: 15,
   },
   nextButtonStyle: {
     width: 200,
@@ -78,6 +114,7 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     backgroundColor: 'rgba(163,51,58,1)',
     justifyContent: 'center',
+    marginTop: 30,
   },
   mainButtonText: {
     color: 'white',
