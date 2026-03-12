3d844c9b01847b7c828d308ba6518ac5152b67d4
alenissmart <alen112.scientist@gmail.com>
2026-03-11 21:55:14 -0400

    Finished done screen and schedule info screen

--- app/(onboarding)/doneScreen.tsx
+++ app/(onboarding)/doneScreen.tsx
@@ -1,62 +1,98 @@
-import { LinearGradient } from 'expo-linear-gradient';
 import { router } from 'expo-router';
 import React from 'react';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Image, Pressable, StyleSheet, Text } from 'react-native';
+import AuthScreenLayout from '../screenTemplate';
 
 const onboardingDoneScreen = () => {
   return (
-    <View style={styles.container}>
-      <LinearGradient colors={['#F5B3B6', '#C94B52']} style={styles.gradient}>
-        <Text style={styles.title}>onboarding Done Screen Template</Text>
-
-        <Pressable
-          onPress={() => {
-            // add function to save user input
-            router.replace('/(tabs)');
-          }}
-          style={({ pressed }) => [
-            styles.nextButtonStyle,
-            {
-              transform: pressed ? [{ scale: 0.95 }] : [{ scale: 1 }],
-            },
-          ]}
-        >
-          <Text style={styles.mainButtonText}>Go to App</Text>
-        </Pressable>
-      </LinearGradient>
-    </View>
+    <AuthScreenLayout
+      headerContent={
+        <>
+          <Text style={styles.title}>{'Everything is set!'}</Text>
+          <Text style={styles.aboutText}>Your new schedule is waiting</Text>
+        </>
+      }
+      middleContent={
+        <>
+          <Image
+            source={require('../../assets/images/tempLogo.png')}
+            style={styles.big_logo}
+          />
+        </>
+      }
+      bottomContent={
+        <>
+          <Pressable
+            onPress={() => {
+              // add function to save user input
+              router.replace('/(tabs)');
+            }}
+            style={({ pressed }) => [
+              styles.nextButton,
+              {
+                transform: pressed ? [{ scale: 0.95 }] : [{ scale: 1 }],
+              },
+            ]}
+          >
+            <Text style={styles.buttonText}>Next</Text>
+          </Pressable>
+          <Pressable
+            onPress={() => {
+              router.back();
+            }}
+          >
+            {({ pressed }) => (
+              <Text
+                style={[
+                  styles.smallText,
+                  {
+                    color: pressed
+                      ? 'rgba(91, 90, 90, 1)'
+                      : 'rgba(182, 179, 179, 1)',
+                    marginTop: 10,
+                  },
+                ]}
+              >
+                Back
+              </Text>
+            )}
+          </Pressable>
+        </>
+      }
+    />
   );
 };
 
 export default onboardingDoneScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-  },
   title: {
     color: 'black',
-    fontSize: 35,
+    fontSize: 40,
     textAlign: 'center',
     fontWeight: '600',
-    marginBottom: 50,
   },
-  gradient: {
-    flex: 1,
-    padding: 15,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 5,
+  aboutText: {
+    color: 'rgba(0, 0, 0, .6)',
+    fontSize: 16,
+    padding: 20,
+    textAlign: 'center',
   },
-  nextButtonStyle: {
+  big_logo: {
+    width: 200,
+    height: 200,
+    borderRadius: 15,
+    marginTop: 80,
+    marginBottom: 100,
+  },
+  nextButton: {
     width: 200,
     height: 60,
     borderRadius: 15,
     backgroundColor: 'rgba(163,51,58,1)',
     justifyContent: 'center',
   },
-  mainButtonText: {
+  buttonText: {
     color: 'white',
     fontSize: 20,
     textAlign: 'center',
@@ -64,7 +100,6 @@ const styles = StyleSheet.create({
   },
   smallText: {
     marginTop: 15,
-    color: 'white',
     fontSize: 18,
     textAlign: 'center',
   },
